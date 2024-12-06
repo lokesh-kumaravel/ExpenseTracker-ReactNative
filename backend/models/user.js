5fd4cd0a10ab0d005ddc42843027333378f5ffce
Lokesh K <lokeshkumaravel29@gmail.com>
2024-12-06 23:10:06 +0530

    commit

--- backend/models/user.js
+++ backend/models/user.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  balance: {
+    type: Number,
+    default: 0,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
